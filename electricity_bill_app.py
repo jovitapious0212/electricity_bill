8a4553d149fc9a08f6fec483a6c785701172d53d
jovitapious0212 <jovitapious0212@gmail.com>
2026-09-18 09:35:34 +0530

    Update electricity_bill_app.py

--- electricity_bill_app.py
+++ electricity_bill_app.py
@@ -1,17 +1,15 @@
 import streamlit as st
-import pickle
+import joblib
 import numpy as np
 
-# Page title
 st.title("Electric Bill Prediction")
 
 st.write("Enter the number of AC Units to predict the Electric Bill.")
 
-# Load the saved model
-with open("electricity_bill_prediction_model.pkl", "rb") as file:
-    model = pickle.load(file)
+# Load model
+model = joblib.load("electricity_bill_prediction_model.pkl")
 
-# Input: greater than 0 and less than 150
+# Input
 ac_units = st.number_input(
     "Enter AC Units",
     min_value=1,
@@ -20,14 +18,11 @@ ac_units = st.number_input(
     step=1
 )
 
-# Prediction button
+# Prediction
 if st.button("Predict Electric Bill"):
 
-    # Convert input into 2D array
     input_data = np.array([[ac_units]])
 
-    # Predict
     prediction = model.predict(input_data)
 
-    # Display result
     st.success(f"Predicted Electric Bill: ₹{prediction[0]:.2f}")
